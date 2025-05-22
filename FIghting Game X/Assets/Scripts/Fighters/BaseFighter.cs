b4436ca60c2ebfc2ee902fcbc1f221b2364bd890
mulsicpp <christianmulser000@gmail.com>
2025-05-22 11:34:47 +0200

    Added stats and grounded functionality

--- FIghting Game X/Assets/Scripts/Fighters/BaseFighter.cs
+++ FIghting Game X/Assets/Scripts/Fighters/BaseFighter.cs
@@ -1,16 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class BaseFighter : MonoBehaviour
 {
     public FighterActions fighter_actions;
-    public Rigidbody2D rigidbody;
+    public new Rigidbody2D rigidbody;
+    public new Collider2D collider;
+
+    public readonly FighterStats stats = FighterStats.DEFAULT;
+
+    public int available_air_jumps;
+
+    private bool _grounded;
+    public bool grounded
+    {
+        set
+        {
+            if (value)
+            {
+                available_air_jumps = stats.air_jumps;
+            }
+            _grounded = value;
+        }
+        get { return _grounded; }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         fighter_actions.init();
 
-        rigidbody = GetComponent<Rigidbody2D>();
+        available_air_jumps = stats.air_jumps;
+
+        // rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,7 +45,46 @@ public class BaseFighter : MonoBehaviour
 
         if(fighter_actions.jump.WasPressedThisFrame())
         {
-            rigidbody.linearVelocityY = 10.0f;
+            if (grounded)
+                jump();
+            else if(available_air_jumps > 0)
+            {
+                jump();
+                available_air_jumps--;
+            }
         }
+    }
+
+    public void FixedUpdate()
+    {
+        grounded = false;
+
+        List<ContactPoint2D> contacts = new List<ContactPoint2D>();
+
+        collider.GetContacts(contacts);
+
+        foreach (var contact in contacts)
+        {
+            handle_contact(contact);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (var contact in collision.contacts)
+            handle_contact(contact);
+    }
+
+    public void handle_contact(ContactPoint2D contact   )
+    {
+        if (contact.normal.y > 0.7f)
+        {
+            grounded = true;
+        }
+    }
+
+    public void jump()
+    {
+        rigidbody.linearVelocityY = stats.jump_strength;
     }
 }
