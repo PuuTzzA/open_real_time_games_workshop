7ad2f23baef54645e08326a5643a426c24c8b14f
mulsicpp <christianmulser000@gmail.com>
2025-06-25 15:51:54 +0200

    implemented jump as delayed action and jab_down boost

--- FIghting Game X/Assets/Scripts/Fighters/Hitbox/AttackHitbox.cs
+++ FIghting Game X/Assets/Scripts/Fighters/Hitbox/AttackHitbox.cs
@@ -10,9 +10,15 @@ public class AttackHitbox : Hitbox
     public override void hit(BaseFighter fighter)
     {
         if(fighter.is_blocking(direction * source_fighter.state.get_facing_ivec())) { return; }
+
         int forced_facing = -direction.x * source_fighter.state.get_facing_int();
         fighter.state.force_facing(forced_facing);
         fighter.knockback(knockback * source_fighter.state.get_facing_vec());
         fighter.take_damage(damage, fighter.gameObject);
+
+        if (direction.Equals(Vector2Int.down))
+        {
+            source_fighter.rigidbody.linearVelocityY = 7.0f;
+        }
     }
 }
