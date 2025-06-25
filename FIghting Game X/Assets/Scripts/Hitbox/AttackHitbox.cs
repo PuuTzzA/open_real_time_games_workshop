7dc2e11bc6287e620187bb4d4e4b5de440cc0f97
mulsicpp <christianmulser000@gmail.com>
2025-06-25 16:24:13 +0200

    added hit type to hitbox

--- FIghting Game X/Assets/Scripts/Hitbox/AttackHitbox.cs
+++ FIghting Game X/Assets/Scripts/Hitbox/AttackHitbox.cs
@@ -7,9 +7,10 @@ public class AttackHitbox : Hitbox
     public Vector2Int direction;
     public BaseFighter source_fighter;
 
-    public override void hit(BaseFighter fighter)
+    public override void hit(BaseFighter fighter, HitType type)
     {
-        if(fighter.is_blocking(direction * source_fighter.state.get_facing_ivec())) { return; }
+        if (type != HitType.Start) { return; }
+        if (fighter.is_blocking(direction * source_fighter.state.get_facing_ivec())) { return; }
 
         int forced_facing = -direction.x * source_fighter.state.get_facing_int();
         fighter.state.force_facing(forced_facing);
--- FIghting Game X/Assets/Scripts/Hitbox/Hitbox.cs
+++ FIghting Game X/Assets/Scripts/Hitbox/Hitbox.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 
+public enum HitType
+{
+    Start,
+    Stay
+}
+
 public class Hitbox : MonoBehaviour {
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -12,13 +18,29 @@ public class Hitbox : MonoBehaviour {
             {
                 case "Fighter":
                     var fighter = obj.transform.parent.gameObject.GetComponent<BaseFighter>();
-                    hit(fighter);
+                    hit(fighter, HitType.Start);
                     break;
             }
         }
     }
 
-    public virtual void hit(BaseFighter fighter)
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        var obj = collision.gameObject;
+
+        if (obj.layer == 7 /*Hurtbox*/)
+        {
+            switch (obj.tag)
+            {
+                case "Fighter":
+                    var fighter = obj.transform.parent.gameObject.GetComponent<BaseFighter>();
+                    hit(fighter, HitType.Stay);
+                    break;
+            }
+        }
+    }
+
+    public virtual void hit(BaseFighter fighter, HitType type)
     {
 
     }
