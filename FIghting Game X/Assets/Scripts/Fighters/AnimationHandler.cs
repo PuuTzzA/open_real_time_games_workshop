d1e32e97e0a4819241191a1052d45f38cc7ead58
mulsicpp <christianmulser000@gmail.com>
2025-06-30 20:06:39 +0200

    added proper animations for idle

--- FIghting Game X/Assets/Scripts/Fighters/AnimationHandler.cs
+++ FIghting Game X/Assets/Scripts/Fighters/AnimationHandler.cs
@@ -3,42 +3,56 @@ using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
-enum AnimationEndAction
+public enum AnimationEndAction
 {
     Loop,
     Wait,
     GoIdle
 }
 
+public class AnimationInfo
+{
+    public string state_name;
+    public AnimationEndAction end_action;
+    public bool idle;
+
+    public AnimationInfo(string state_name, AnimationEndAction end_action, bool idle = false)
+    {
+        this.state_name = state_name;
+        this.end_action = end_action;
+        this.idle = idle;
+    }
+}
+
 public class AnimationHandler : MonoBehaviour
 {
     public Animator animator;
-    private static readonly (string, AnimationEndAction)[] state_infos = {
-        ("idle", AnimationEndAction.Loop),
-        ("running", AnimationEndAction.Loop),
-        ("jump", AnimationEndAction.GoIdle),
-        ("jab_up", AnimationEndAction.GoIdle),
-        ("jab_side", AnimationEndAction.GoIdle),
-        ("jab_down", AnimationEndAction.GoIdle),
-        ("falling", AnimationEndAction.Loop),
-        ("ult", AnimationEndAction.GoIdle),
-        ("emote", AnimationEndAction.GoIdle),
-        ("block_up", AnimationEndAction.Wait),
-        ("block_side", AnimationEndAction.Wait),
-        ("heavy_up", AnimationEndAction.GoIdle),
-        ("heavy_side", AnimationEndAction.GoIdle),
-        ("heavy_down", AnimationEndAction.GoIdle),
-        ("dash", AnimationEndAction.GoIdle),
-        ("knocked_back_light", AnimationEndAction.GoIdle),
-        ("knocked_back_heavy", AnimationEndAction.GoIdle),
-        ("stunned", AnimationEndAction.GoIdle),
-        ("die", AnimationEndAction.GoIdle)
+    private static readonly AnimationInfo[] state_infos = {
+        new ("idle", AnimationEndAction.Loop, true),
+        new ("running", AnimationEndAction.Loop, true),
+        new ("jump", AnimationEndAction.GoIdle),
+        new ("jab_up", AnimationEndAction.GoIdle),
+        new ("jab_side", AnimationEndAction.GoIdle),
+        new ("jab_down", AnimationEndAction.GoIdle),
+        new ("falling", AnimationEndAction.Loop, true),
+        new ("ult", AnimationEndAction.GoIdle),
+        new ("emote", AnimationEndAction.GoIdle),
+        new ("block_up", AnimationEndAction.Wait),
+        new ("block_side", AnimationEndAction.Wait),
+        new ("heavy_up", AnimationEndAction.GoIdle),
+        new ("heavy_side", AnimationEndAction.GoIdle),
+        new ("heavy_down", AnimationEndAction.GoIdle),
+        new ("dash", AnimationEndAction.GoIdle),
+        new ("knocked_back_light", AnimationEndAction.GoIdle),
+        new ("knocked_back_heavy", AnimationEndAction.GoIdle),
+        new ("stunned", AnimationEndAction.GoIdle),
+        new ("die", AnimationEndAction.GoIdle)
     };
 
     private int _current_frame_count;
     private int _frame_index;
     private FighterAction _action;
-    private AnimationEndAction _end_action;
+    private AnimationInfo _info;
 
     private void Awake()
     {
@@ -47,43 +61,42 @@ public class AnimationHandler : MonoBehaviour
 
     public void play(FighterAction action)
     {
-        var (state_name, end_action) = state_infos[(int)action];
+        _info = state_infos[(int)action];
         _action = action;
-        animator.Play(state_name, 0, 0.0f);
+        animator.Play(_info.state_name, 0, 0.0f);
         animator.Update(0.0f);
 
         var clip = animator.GetCurrentAnimatorClipInfo(0)[0].clip;
         _current_frame_count = Mathf.RoundToInt(clip.frameRate * clip.length);
-        _end_action = end_action;
         _frame_index = 0;
     }
 
     public FighterAction get_action() { return _action; }
+    public AnimationInfo get_info() { return _info; }
 
-    public void tick()
+    public bool tick()
     {
-        switch (_end_action)
+        switch (_info.end_action)
         {
             case AnimationEndAction.Loop:
                 _frame_index %= _current_frame_count;
                 break;
             case AnimationEndAction.Wait:
-                if (_frame_index < 0)
-                    _frame_index = 0;
                 if (_frame_index >= _current_frame_count)
                     _frame_index = _current_frame_count - 1;
                 break;
             case AnimationEndAction.GoIdle:
                 if (_frame_index >= _current_frame_count)
                 {
-                    play(FighterAction.Idle);
+                    return true;
                 }
                 break;
         }
 
         float normalized_time = _frame_index / (float)_current_frame_count;
-        animator.Play(state_infos[(int)_action].Item1, 0, normalized_time);
+        animator.Play(_info.state_name, 0, normalized_time);
         animator.Update(0.0f);
         _frame_index++;
+        return false;
     }
 }
