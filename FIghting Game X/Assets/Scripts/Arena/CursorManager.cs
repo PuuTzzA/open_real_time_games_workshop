de976687d72383432d8fa5e0790481e0d9fdbc10
PuuTzzA <putzerthomas03@gmail.com>
2025-07-22 21:16:40 +0200

    fixed framerate

--- FIghting Game X/Assets/Scripts/Arena/CursorManager.cs
+++ FIghting Game X/Assets/Scripts/Arena/CursorManager.cs
@@ -6,5 +6,9 @@ public class CursorManager : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;  // Locks the cursor to the center of the screen
         Cursor.visible = false;                    // Hides the cursor
+
+        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0; // Disable VSync so Application.targetFrameRate takes effect
     }
+
 }
